0d12e8bd4481ac70226da6a959a5e4588fca00f2
Kirtiman Singh <kirt93@hotmail.com>
2018-10-29 16:12:28 +0000

    Extract the responsibility for attacking players from the Player class into a Game class.

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require './lib/player.rb'
+require './lib/game.rb'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -22,7 +23,7 @@ end
 get '/attack' do
   @player_1 = $player_1
   @player_2 = $player_2
-  @player_1.attack(@player_2)
+  Game.new.attack(@player_2)
   erb :attack
 end
   # start the server if ruby file executed directly
