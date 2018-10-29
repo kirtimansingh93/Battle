8b0b2db097bd437861610c463ad31018e27773a4
Kirtiman Singh <kirt93@hotmail.com>
2018-10-29 15:23:15 +0000

    Passing unit tests - player can be attacked and receive damage

--- lib/player.rb
+++ lib/player.rb
@@ -6,4 +6,12 @@ class Player
     @name = name
     @hit_points = hit_points
   end
+
+  def attack(player)
+    player.receive_damage
+  end
+
+  def receive_damage
+    @hit_points -= 10
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,14 +3,29 @@ require 'player'
 describe Player do
   subject(:kirt) { Player.new('Kirt') }
   subject(:edward) { Player.new('Edward') }
+
   describe '#name' do
     it 'returns the name' do
       expect(kirt.name).to eq 'Kirt'
     end
   end
+
   describe '#hit_points' do
    it 'returns the hit points' do
      expect(kirt.hit_points).to eq described_class::DEFAULT_HIT_POINTS
    end
  end
+
+ describe '#attack' do
+    it 'damages the player' do
+      expect(edward).to receive(:receive_damage)
+      kirt.attack(edward)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { edward.receive_damage }.to change { edward.hit_points }.by(-10)
+    end
+  end
 end
