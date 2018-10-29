811c8ce89435e1d25b258e5bc2a7bb684ed0d46c
Kirtiman Singh <kirt93@hotmail.com>
2018-10-29 16:28:15 +0000

    Skinny controllers - Access player instances through Game class

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -7,7 +7,7 @@ feature 'Attacking' do
   scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
     click_button 'Attack'
-    # click_link 'OK'
+    click_link 'OK'
     expect(page).not_to have_content 'Edward: 60HP'
     expect(page).to have_content 'Edward: 50HP'
   end
